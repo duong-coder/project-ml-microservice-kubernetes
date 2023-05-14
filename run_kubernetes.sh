f1cc33ac90d438602043af8434ac3ec25a443dff
DuongNH <duongakvip1@gmail.com>
2023-05-14 17:10:54 +0700

    Task 6: Deploy with Kubernetes and Save Output Logs

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=duongnhdocker
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deploy ml-microservice-kubernetes-minikube --image="$dockerpath/ml-microservice-kubernetes"
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment.apps/ml-microservice-kubernetes-minikube 8000:80
