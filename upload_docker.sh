a55eee5b00b94d053d47759ec7fdfd77844bc47d
DuongNH <duongakvip1@gmail.com>
2023-05-14 16:42:39 +0700

    Task 4: Upload the Docker Image

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=duongnhdocker
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+cat ./my_password.txt | docker login --username "$dockerpath" --password-stdin
+docker image tag "ml-microservice-kubernetes" "$dockerpath/ml-microservice-kubernetes"
 
 # Step 3:
 # Push image to a docker repository
+docker image push "$dockerpath/ml-microservice-kubernetes"
